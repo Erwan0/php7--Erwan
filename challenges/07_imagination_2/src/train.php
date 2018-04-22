599217bd480e4fab68c2cf8050c6fdc93db6d003
Erwan0 <37361461+Erwan0@users.noreply.github.com>
2018-04-22 10:13:41 +0200

    Revert "07_imagination_2"
    This reverts commit 360c08f252a74fb3d7127071979c107f3e6b758c.

--- challenges/07_imagination_2/src/train.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class Train extends Vehicule
-{
-    public function __toString()
-    {
-        return "C'est un  : ". $this->name;
-    }
-}
--- challenges/07_imagination_2/src/voiture.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class Voiture extends Vehicule
-{
-    public function __toString()
-    {
-        return "C'est une  : ". $this->name;
-    }
-}
